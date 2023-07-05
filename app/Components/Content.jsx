4f8dfb161b8872eae896e8ec0df11dabd47a705c
DumeGrisoni <misanthrope20218@gmail.com>
2023-07-05 10:20:58 +0200

    Adding display on mouse enter on Content

--- app/Components/Content.jsx
+++ app/Components/Content.jsx
@@ -1,15 +1,29 @@
 'use client';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Content = ({ src }) => {
+  const [display, setDisplay] = useState(false);
+
   return (
-    <div className="flex flex-col justify-start items-center">
-      <h2>Content 1</h2>
+    <div
+      className="relative"
+      onMouseEnter={() => setDisplay(true)}
+      onMouseLeave={() => setDisplay(false)}
+    >
+      {display && (
+        <div className="absolute bottom-0  h-[50%] w-[100%] content-bg ">
+          <div className="flex-col justify-center items-center text-center h-[100%] p-4">
+            <div className="mt-2 text-lg">Artiste</div>
+            <div className="text-sm">Titre</div>
+          </div>
+        </div>
+      )}
       <Image
         src={src}
         width={200}
         height={200}
-        className="lg:h-[200px] lg:w-[200px] h-auto w-auto"
+        className=" w-[200px] h-[200px] "
         alt="image"
       />
     </div>
